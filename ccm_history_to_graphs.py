32857034137821ff81b733d04bfca0efd2c9e34c
Aske Olsson <askeolsson@gmail.com>
2011-03-08 21:32:22 +0100

    Clean up graphing files
    Deleted the old CCMHistoryGraph.py which was made as a class af first, but a module could do

--- ccm_history_to_graphs.py
+++ ccm_history_to_graphs.py
@@ -11,9 +11,11 @@ Copyright (c) 2011 Nokia. All rights reserved.
 
 import FileObject
 import TaskObject
-import CCMHistoryGraph as ccm_graph
 import pygraphviz as gv
+import convert_history as ch
 from pygraphviz import *
+from pygraph.classes.digraph import digraph
+from pygraph.classes.hypergraph import hypergraph
 
 def create_graphs_from_releases(releases):
     # Find first release i.e. where previous is none
@@ -28,7 +30,7 @@ def create_graphs_from_releases(releases):
     while release:
         #print "Creating graph for", release
         graphs[release] = {}
-        object_graph, task_graph, release_graph, commit_graph = ccm_graph.create_graphs(releases[release])
+        object_graph, task_graph, release_graph, commit_graph = create_graphs(releases[release])
         graphs[release]['commit'] = commit_graph
         graphs[release]['task'] = task_graph
         graphs[release]['object'] = object_graph
@@ -44,6 +46,107 @@ def create_graphs_from_releases(releases):
 
 
     return graphs
+
+
+
+
+def find_objects_without_associated_tasks(objects, tasks):
+    objects_from_tasks = []
+    # compare objects in the tasks with objects in release, to see if there is any single objects
+    object_names= set([o.get_object_name() for o in objects])
+    objects_from_tasks = set([o for task in tasks for o in task.get_objects()])
+    single_object_names = object_names - objects_from_tasks
+    single_objects = [o for o in objects if o.get_object_name() in single_object_names]
+    return single_objects
+
+def get_commit_history(release):
+    commits = create_graphs(release)
+
+    return commits
+
+def create_graphs(release):
+    tasks = release['tasks']
+    objects = release['objects']
+
+    object_graph = create_object_graph(objects)
+    task_graph = create_task_graph(tasks, objects)
+    release_graph = create_release_graph(objects, release['name'], release['previous']);
+    commit_graph = ch.convert_history(object_graph, task_graph, release_graph, objects)
+
+    return object_graph, task_graph, release_graph, commit_graph
+
+
+def create_release_graph(objects, release, previous):
+    release_graph = hypergraph()
+    release_graph.add_nodes([o.get_object_name() for o in objects])
+    release_graph.add_edges([release, previous])
+
+    object_names = [o.get_object_name() for o in objects]
+    for o in objects:
+        # Bind objects to this release
+        if o.get_successors() is None:
+            #print "linking", o.get_object_name(), "to release", release
+            release_graph.link(o.get_object_name(), release)
+
+        # Bind objects to previous release
+        predecessors = o.get_predecessors()
+        if predecessors is not None:
+            for p in predecessors:
+                if p not in object_names:
+                    if not release_graph.has_node(p):
+                        release_graph.add_node(p)
+                        #print "linking", p, "to release", previous
+                        release_graph.link(p, previous)
+
+    return release_graph
+
+
+def create_task_graph(tasks, objects):
+    task_graph = hypergraph()
+    task_graph.add_nodes([o.get_object_name() for o in objects])
+    task_graph.add_hyperedges([t.get_object_name() for t in tasks])
+    #link the objects and the tasks
+    for t in tasks:
+        for o in t.get_objects():
+            #print "linking:", o, "and", t.get_object_name()
+            task_graph.link(o, t.get_object_name())
+    # Add single_objects to task_graph
+    for o in find_objects_without_associated_tasks(objects, tasks):
+        task_graph.add_hyperedge(o.get_object_name())
+        #print "linking:", o.get_object_name(), "and", o.get_object_name()
+        task_graph.link(o.get_object_name(), o.get_object_name())
+
+    return task_graph
+
+
+def create_object_graph(objects):
+    # create dict to map objectname to file object
+    mapped_objects = {}
+    for o in objects:
+        mapped_objects[o.get_object_name()] = o
+    object_graph = digraph()
+    object_graph.add_nodes([o.get_object_name() for o in objects])
+    # Create relationship list
+    successors = [(i.get_object_name(), [] if i.get_successors() is None else [mapped_objects[j] for j in i.get_successors()] ) for i in objects]
+    #
+    for obj, suc in successors:
+        for s in suc:
+            object_graph.add_edge((obj, s.get_object_name()))
+
+    object_names = [o.get_object_name() for o in objects]
+    for o in objects:
+        # Bind objects to previous release
+        predecessors = o.get_predecessors()
+        if predecessors is not None:
+            for p in predecessors:
+                if p not in object_names:
+                    if not object_graph.has_node(p):
+                        object_graph.add_node(p)
+                        object_graph.add_edge((p,o.get_object_name()))
+
+    return object_graph
+
+
 
 def object_graph_to_image(object_graph, release):
 
