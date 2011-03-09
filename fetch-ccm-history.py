6f00e5ea7d86c0069ca66470628d94d2fb732803
Knud Poulsen <knud.poulsen@nokia.com>
2011-03-09 21:47:09 +0100

    parallelize bulk ccm operations, approx 3x speedup

--- fetch-ccm-history.py
+++ fetch-ccm-history.py
@@ -9,14 +9,6 @@ Created by Emanuele Zattin and Aske Olsson on 2011-01-26.
 Copyright (c) 2011 Nokia. All rights reserved.
 """
 
-import SynergySession
-import FileObject
-import TaskObject
-import SynergyObject
-from SynergyUtils import ObjectHistory, TaskUtil, SynergyUtils
-
-from operator import itemgetter, attrgetter
-
 from datetime import datetime
 import time
 import cPickle
@@ -24,11 +16,35 @@ import os.path
 import os
 import sys
 
+import SynergySession
+import SynergySessions
+import FileObject
+import TaskObject
+import SynergyObject
+from SynergyUtils import ObjectHistory, TaskUtil, SynergyUtils, ObjectHistoryPool
+
+from operator import itemgetter, attrgetter
+
+class Timer():
+    def __init__(self):
+        self.time = []
+    def __enter__(self):
+        self.start = time.time()
+    def __exit__(self, *args):
+        end = time.time() - self.start
+        self.time.append(end)
+        if len(self.time) % 100 == 0:
+            print "time used processing last 100 objects:", sum(self.time[-100:])
+        if len(self.time) % 1000 == 0:
+            print "time used processing last 1000 objects:", sum(self.time[-1000:])
+            self.time = []
+
 class CCMHistory(object):
     """Get History (objects and tasks) in a Synergy (ccm) database between baseline projects"""
 
-    def __init__(self, ccm, history, outputfile):
+    def __init__(self, ccm, ccmpool, history, outputfile):
         self.ccm = ccm
+        self.ccmpool = ccmpool
         self.delim = self.ccm.delim()
         self.history = history
         self.history_created = []
@@ -131,15 +147,23 @@ class CCMHistory(object):
     def find_project_diff(self, latestproject, baseline_project, toplevel_project):
         # Find difference between latestproject and baseline_project
         if baseline_project:
-            object_hist = ObjectHistory(self.ccm, toplevel_project, baseline_project)
+            object_hist_pool = ObjectHistoryPool(self.ccmpool, toplevel_project, baseline_project)
             objects_changed = self.ccm.query("recursive_is_member_of('{0}', 'none') and not recursive_is_member_of('{1}', 'none')".format(latestproject, baseline_project)).format("%objectname").format("%owner").format("%status").format("%create_time").format("%task").run()
         else:
             # root project, get ALL objects in release
-            object_hist = ObjectHistory(self.ccm, toplevel_project, toplevel_project)
+            object_hist_pool = ObjectHistoryPool(self.ccmpool, toplevel_project, toplevel_project)
             objects_changed = self.ccm.query("recursive_is_member_of('{0}', 'none')".format(latestproject)).format("%objectname").format("%owner").format("%status").format("%create_time").format("%task").run()
 
+        print "DEBUG: instantiated an object_hist_pool, type is: " + str(type(object_hist_pool))
+        print "DEBUG: object_hist_pool[0]'s type is: " + str(type(object_hist_pool[0]))
+
+        # make the objects_changed dictionary into an array, to be able to walk it by index
+        objects_changed_indexable = []
+        for o in objects_changed:
+            objects_changed_indexable.append(o)
+
         num_of_objects = len([o for o in objects_changed if ":project:" not in o])
-        print "objects to process for",  latestproject, num_of_objects
+        print "objects to process for",  latestproject, ": ", num_of_objects
         objects = {}
         persist = 1
         if self.tag in self.history.keys():
@@ -151,23 +175,69 @@ class CCMHistory(object):
             self.history[self.tag] = {'objects': [], 'tasks': []}
 
         # Check history for all objects and add them to history
-        for o in objects_changed:
-            #print o['objectname']
-            if o['objectname'] not in objects.keys():
-                # Don't do project objects
-                if ':project:' not in o['objectname']:
-                    with self.timer:
-                        objects.update(object_hist.get_history(FileObject.FileObject(o['objectname'], self.delim, o['owner'], o['status'], o['create_time'], o['task'])))
-                    persist +=1
-            else:
-                print o['objectname'], "already in history"
+        objects_changed_processing_progress_idx = 0
+        while True: # this is the objects_changed progress outer loop, which is broken when the last object in objects_changed has been processed in a processing pool
+            # build the processing pool
+            current_pool_fill_idx = 0
+            current_pool_ObjectArray = []
+            print "DEBUG: Populate a processing pool..."
+            while True:
+                tempobject = objects_changed_indexable[objects_changed_processing_progress_idx]
+                print "DEBUG: -  start checking: tempobject['objectname'] = " + str(tempobject['objectname']) + " for pool inclusion"
+                if tempobject['objectname'] not in objects.keys(): 
+                    print "DEBUG: --  tempobject['objectname'] = " + str(tempobject['objectname']) + " not already in objects.keys()"
+                    if ':project:' not in tempobject['objectname']:
+                        print "DEBUG: ---  ':project:' not in tempobject['objectname'] = " + str(tempobject['objectname'])
+                        print "DEBUG:      objects_changed_indexable[objects_changed_processing_progress_idx] type = " + str(type(objects_changed_indexable[objects_changed_processing_progress_idx]))
+                        current_pool_ObjectArray.append(objects_changed_indexable[objects_changed_processing_progress_idx])
+                        print "DEBUG: current_pool_fill_idx = " + str(current_pool_fill_idx)                       
+                        print "DEBUG: ----  current_pool_ObjectArray[" + str(current_pool_fill_idx) + "] = " + str(current_pool_ObjectArray[current_pool_fill_idx])
+                        if (current_pool_fill_idx >= self.ccmpool.nr_sessions-1):
+                            break
+                        current_pool_fill_idx += 1
+                if (objects_changed_processing_progress_idx >= len(objects_changed_indexable)-1):
+                    break
+                objects_changed_processing_progress_idx += 1
+    
+            print "DEBUG: start processing pool"
+            ccm_session_idx = 0
+            print "DEBUG: type(object_hist_pool) = " + str(type(object_hist_pool))
+            print "DEBUG: type(ccm_session_idx) = " + str(type(ccm_session_idx))
+            for workobject in current_pool_ObjectArray:
+                print "DEBUG: type(workobject) = " + str(type(workobject))
+                # now we have a processing pool, of finite size, kick off the pool
+                print "DEBUG: +  object_hist_pool[" + str(ccm_session_idx) + "].start_get_history("
+                object_hist_pool[ccm_session_idx].start_get_history(FileObject.FileObject(workobject['objectname'], self.delim, workobject['owner'], workobject['status'], workobject['create_time'], workobject['task']))
+                ccm_session_idx += 1
+
+            # join the processing pool
+            current_pool_ReturnObjectsArray = []
+            with self.timer:
+                ccm_session_idx = 0
+                for workobject in current_pool_ObjectArray:
+                    print "DEBUG: Waiting for object_hist_pool[" + str(ccm_session_idx) + "]"
+                    current_pool_ReturnObjectsArray.append(object_hist_pool[ccm_session_idx].join_get_history())
+                    ccm_session_idx += 1
+            
+            # update the objects not already in the objects array (in case the same object is effectively processed multiple times in the same pool)
+            for retObject in current_pool_ReturnObjectsArray:
+                print "retObject = " + str(retObject)
+                print "retObject type = " + str(type(retObject))
+                if (retObject not in objects.keys()):
+                    objects.update(retObject)
+
+            # break out of the pool processing loop if all elemenets have been processed
+            if (objects_changed_processing_progress_idx >= len(objects_changed_indexable)-1):
+                break
+
+            persist += len(current_pool_ReturnObjectsArray)
 
             if persist % 100 == 0:
                 self.history[self.tag]['objects'] = objects.values()
                 fname = self.outputfile + '_' + self.tag + '_inc'
                 self.persist_data(fname, self.history[self.tag])
 
-            num_of_objects -= 1
+            num_of_objects -= len(current_pool_ObjectArray)
             print "objects left:", num_of_objects
 
 
@@ -244,32 +314,15 @@ class CCMHistory(object):
         fh.close()
         print "done..."
 
-class Timer():
-    def __init__(self):
-        self.time = []
-    def __enter__(self):
-        self.start = time.time()
-    def __exit__(self, *args):
-        end = time.time() - self.start
-        self.time.append(end)
-        if len(self.time) % 100 == 0:
-            print "time used processing last 100 objects:", sum(self.time[-100:])
-        if len(self.time) % 1000 == 0:
-            print "time used processing last 1000 objects:", sum(self.time[-1000:])
-            self.time = []
-
 
 def main():
-    # make all stdout stderr writes unbuffered/ instant/ inline
-    sys.stdout =  os.fdopen(sys.stdout.fileno(), 'w', 0);
-    sys.stderr =  os.fdopen(sys.stderr.fileno(), 'w', 0);
-
     ccm_db = sys.argv[1]
     project = sys.argv[2]
     outputfile = sys.argv[3]
 
     print "Starting Synergy session on", ccm_db, "..."
     ccm = SynergySession.SynergySession(ccm_db)
+    ccmpool = SynergySessions.SynergySessions(database=ccm_db, nr_sessions=10)
     print "session started"
     delim = ccm.delim()
     history = {}
@@ -282,8 +335,7 @@ def main():
         print "history contains:", history.keys()
 
     #print history
-    fetch_ccm = CCMHistory(ccm, history, outputfile)
-
+    fetch_ccm = CCMHistory(ccm, ccmpool, history, outputfile)
     history = fetch_ccm.get_project_history(project)
 
 
