b715d1acd2f98085f1901ab3e5ea5290eca963a5
Aske Olsson <askeolsson@gmail.com>
2011-03-21 15:27:58 +0100

    Updated the test (main) in SynergySessions
    Now the test uses the multiprocessing package instead of the old threading functionallity in SynergySession.

--- SynergySessions.py
+++ SynergySessions.py
@@ -3,18 +3,19 @@
 """
 SynergySessions.py
 
+Created by Knud Poulsen Aske Olsson on 2011-03-08.
 Copyright (c) 2011, Nokia
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
 Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the 
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
 distribution.
 Neither the name of the Nokia nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
-FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
@@ -25,7 +26,7 @@ import random
 from datetime import datetime, timedelta
 from subprocess import Popen, PIPE
 import SynergySession
-from Queue import Queue
+from multiprocessing import Process, Queue
 
 sys.stdout =  os.fdopen(sys.stdout.fileno(), 'w', 0);
 sys.stderr =  os.fdopen(sys.stderr.fileno(), 'w', 0);
@@ -41,14 +42,12 @@ class SynergySessions(object):
         self.engine = engine
         self.nr_sessions = nr_sessions
         self.max_session_index = nr_sessions-1
-        
         """populate and array with synergy sessions"""
         self.sessionArray = {}
         for i in range (self.nr_sessions):
-            print "starting session [" + str(i) + "]"
             self.sessionArray[i] = SynergySession.SynergySession(self.database, self.engine, self.command_name, self.ccm_ui_path, self.ccm_eng_path)
             self.sessionArray[i].setSessionID(i)
-            
+            print "started ccm session [" + str(i) + "]",  self.sessionArray[i].environment['CCM_ADDR'].strip()
 
     def __getitem__(self, index):
         if ((index > self.max_session_index) or (index < 0)):
@@ -62,8 +61,10 @@ class SynergySessions(object):
             retstring = retstring + "[" + str(i) + "] " + self.sessionArray[i].getCCM_ADDR() + "\n"
         return retstring
 
- 
 
+def do_query(task, ccm, queue):
+    result = ccm.query("is_associated_cv_of(task('%s'))" % task).format("%objectname").run()
+    queue.put(result)
 
 def main():
     """Test: start a bunch of sessions in parallel, start commands on each, wait for them all to return and print the result in execution order"""
@@ -72,7 +73,7 @@ def main():
     print "ccmpool:"
     print ccmpool
 
-    nr_ops_in_test = 11
+    nr_ops_in_test = 51
 
     print "asking for info on " + str(nr_ops_in_test) + " random tasks in parallel"
     tstart = datetime.now()
@@ -82,29 +83,49 @@ def main():
     ops_count = 0
 
     for i in range(nr_full_parallel_run_groups):
+        processes = []
+        queues = []
         for j in range(ccmpool.nr_sessions):
             ops_count+=1
             querytask = random.randint(1000, 100000)
             print "[" + str(j) + "] ops_count=" + str(ops_count) + ", task=" + str(querytask)
-            ccmpool[j].query("is_associated_cv_of(task('co1asset#%i'))" % querytask).format("%objectname").start()
-        for j in range(ccmpool.nr_sessions):
-            print "DEBUG: .join() waiting for [" + str(j) + "]" 
-            results = ccmpool[j].join()
-            print results
+            task = "co1asset#%i" % querytask
+            queues.append(Queue())
+            processes.append(Process(target=do_query, args=(task, ccmpool[j], queues[j])))
 
+        for p in processes:
+            p.start()
+
+        for j in range(ccmpool.nr_sessions):
+            print "DEBUG: .join() waiting for [" + str(j) + "]"
+            res = queues[j].get()
+            processes[j].join()
+            print res
+
+    #reset processes
+    processes = []
     for j in range(partial_parallel_run_nr):
+        processes = []
         ops_count+=1
         querytask = random.randint(1000, 100000)
+        task = "co1asset#%i" % querytask
         print "[" + str(j) + "] ops_count=" + str(ops_count) + ", task=" + str(querytask)
-        ccmpool[j].query("is_associated_cv_of(task('co1asset#%i'))" % querytask).format("%objectname").start()
+        queues.append(Queue())
+        processes.append(Process(target=do_query, args=(task, ccmpool[j], queues[j])))
+        #ccmpool[j].query("is_associated_cv_of(task('co1asset#%i'))" % querytask).format("%objectname").start()
+
+    for p in processes:
+        p.start()
+
     for j in range(partial_parallel_run_nr):
         print "DEBUG: .join() waiting for [" + str(j) + "]"
-        results = ccmpool[j].join()
-        print results
-    
+        res = queues[j].get()
+        processes[j].join()
+        print res
+
     tparallel = datetime.now()-tstart
     print str(nr_ops_in_test) + " operations, in parallel, on " + str(ccmpool.nr_sessions) + " ccm sessions, it took: " + str(tparallel.seconds) + " seconds"
-    
+
 if __name__ == '__main__':
     main()
 
