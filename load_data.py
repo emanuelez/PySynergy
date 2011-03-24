e856564d57f05a68ac487c13c2b5a9e8ae893632
Aske Olsson <askeolsson@gmail.com>
2011-03-24 15:11:19 +0100

    Remove CCMHistoryGraph from import

--- load_data.py
+++ load_data.py
@@ -14,7 +14,6 @@ import TaskObject
 import cPickle
 f = open('s30_hist.p', 'rb')
 history = cPickle.load(f)
-import CCMHistoryGraph
 import convert_history as ch
 import ccm_history_to_graphs as cg
 import ccm_fast_export as cfe
